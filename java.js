751349410b346e272b326c46e91d9e1ba1d90848
lNurdanl <115089718+lNurdanl@users.noreply.github.com>
2022-10-08 17:46:00 +0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,17 @@
+//Задача 1 (while)
+let i = 1;
+while (i <= 50) {
+    document.write(i + '<br>');
+    i++;
+}
+
+
+//Задача 1 (for)
+for (let i = 1; i <= 50; i++) {
+    document.write(i + '<br>');
+}
+
+//Задача 2
+let arr = ['Привет, ', 'Мир', '!'];
+alert(arr[0] + arr[1] + arr[2]);
+console.log(arr)
